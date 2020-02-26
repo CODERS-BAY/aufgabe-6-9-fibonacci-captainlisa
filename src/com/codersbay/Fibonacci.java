eb78482310bd0dfc91d8726b7e758ecb5359da7b
Lisa-Maria Plakolb <lisa.plakolb96@gmx.at>
2020-02-26 11:17:30 +0100

    Add Fibonacci solution using recursion

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -1,9 +1,24 @@
 package com.codersbay;
+import java.util.Scanner;
 
 public class Fibonacci {
 
     public static void main(String[] args) {
+
         System.out.println("Hello Fibonacci");
+        System.out.println("Enter a number:");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(Fibonacci(n));
+    }
+
+    public static Integer Fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return Fibonacci(n-1) + Fibonacci(n-2);
     }
 
 }
